8a11528790f3a4b7d018acad75ee0b77f6bdc024
Kenawell, Adam (A.) <akenawel@ford.com>
2026-05-13 14:41:15 -0400

    cp1: initial scaffolding - Django project with 4 apps

---
+++ ai_worker/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AiWorkerConfig(AppConfig):
+    name = 'ai_worker'
---
+++ core_infra/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoreInfraConfig(AppConfig):
+    name = 'core_infra'
